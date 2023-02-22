3e811c03cf49c4504fcd25e1949eaa96ecee2809
Sirojjuddin Apendi <60147575+siroj05@users.noreply.github.com>
2023-02-22 07:52:06 +0000

    length character validation

--- my-remix-app/app/components/NewNote.tsx
+++ my-remix-app/app/components/NewNote.tsx
@@ -1,8 +1,18 @@
+import { Form, useTransition as useNavigation, useActionData } from "@remix-run/react";
 import styles from "./NewNote.css"
 
 function NewNote () {
+
+    const data = useActionData()
+
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === "submitting";
+
     return (
-        <form method="post" id="note-form">
+        <Form method="post" id="note-form">
+            {
+                data?.message && <p>{data.message}</p>
+            }
             <p>
                 <label htmlFor="title">Title</label>
                 <input type="text" name="title" id="title" />
@@ -12,9 +22,11 @@ function NewNote () {
                 <textarea name="content" id="content" rows={5}></textarea>
             </p>
             <div className="form-actions">
-                <button>Add Note</button>
+                <button disabled={isSubmitting} >
+                    {isSubmitting? "Adding..." : "Add Note"}
+                </button>
             </div>
-        </form>
+        </Form>
     )
 }
 
--- my-remix-app/app/routes/notes.tsx
+++ my-remix-app/app/routes/notes.tsx
@@ -2,26 +2,42 @@ import NewNote, {links as NewNoteStyle} from "~/components/NewNote"
 import NoteList, {links as NoteListLinks} from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/data";
 import { redirect } from "@remix-run/node";
+import { useActionData, useLoaderData } from "@remix-run/react";
 
 
 export default function notesPage(){
+
+    const notes = useLoaderData();
+    // const data = useActionData();
+
     return(
         <main>
             <NewNote/>
-            <NoteList/>
+            <NoteList notes={notes}/>
         </main>
     )
 }
 
+export async function loader(){
+    const notes = await getStoredNotes();
+    return notes
+}
+
+
 export async function action({request} : any){
     const formData = await request.formData();
     const noteData = Object.fromEntries(formData)
+    if(noteData.title.trim().length < 5){
+        return {message : 'invalid title - must be atleast 5 character long'}
+    }
+
     // console.log(noteData);
     // add validation...
     const existingNotes = await getStoredNotes()
     noteData.id = new Date().toISOString();
     const updateNotes = existingNotes.concat(noteData)
     await storeNotes(updateNotes)
+    // await new Promise<void>((resolve, reject)=>setTimeout(()=>resolve(), 1000))
     return redirect("/notes")
 }
 
